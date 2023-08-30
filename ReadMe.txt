d302958e9be44aff9beece78b4e8a0cec8b3a64b
Faja.rS <muhammadfajar1183@gmail.com>
2023-08-30 22:44:30 +0700

    create introduction material

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+Golang JSON Syllabus:
+
+1. JSON Introduction (json_introduction_test.go)
---
+++ json_introduction_test.go
@@ -0,0 +1,22 @@
+package main
+
+/**
+ * Intro JSON
+ * JSON acronym from Javascript Object Notation, which is
+ * a structured format data which forms like an Object in Javascript
+ *
+ * JSON is a structure format data which mostly used, when we create
+ * RESTful API
+ *
+ * for more information about JSON:
+ * https://www.json.org/json-en.html
+ */
+
+/**
+ * Package JSON
+ * Golang already has package json, where we can use this package for do
+ * conversion data to JSON (encode) or otherwise (decode)
+ *
+ * for more information about package JSON:
+ * https://pkg.go.dev/encoding/json
+ */
